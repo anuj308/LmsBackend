9f6fd586e8170438b117b7f266ff84cc288a65c1
Anuj kumar sharma <141046905+anuj308@users.noreply.github.com>
2024-12-26 15:54:38 +0530

    Rating model created for calculating average rating

---
+++ models/Rating.model.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const RatingSchema = new mongoose.Schema(
+    {
+        courseId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Course",
+            required: [true, "Course is required"],
+        },
+        UserId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: [true, "User is required"],
+        },
+        comment: {
+            type: String,
+            trim: true,
+            required: [true, "Comment is required"],
+        },
+        rating: {
+            type: Number,
+            min: [0, "Rating should At least be 0"],
+            max: [5, "Rating can not be more than 5"],
+        },
+    },
+    {
+        timestamps: true,
+        toJSON: { virtuals: true },
+        toObject: { virtuals: true },
+    }
+);
+
+export const Rating = mongoose.model("Rating", RatingSchema);
